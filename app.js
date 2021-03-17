2a32a57febbcdd675523f82ed52439245b477942
hanan omar altobasi <hananaltobase96@gmail.com>
2021-03-17 23:02:36 +0200

    finish lab-13

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ for(let i=0;i<names.length;i++){
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+randomNumber();
 
 //[0,1,2,....,19]
  function render(){
@@ -57,15 +58,16 @@ render();
  let ListResult=document.getElementById('ListResult');
  let rounds=25;
  let counter=0;
-
  imgSection.addEventListener('click',clickHandler);
 
  function clickHandler(event){
+  
+  counter+=1;
+
     if (event.target.id === 'leftImg' || event.target.id === 'centerImg' || event.target.id === 'rightImg'){
       for(let i=0;i<Products.all.length;i++){
         if (Products.all[i].name === event.target.title){
             Products.all[i].votes++;
-            counter++;
           console.table(Products.all[i]);
         }
       }
@@ -73,30 +75,58 @@ render();
       render();
 
     if(counter===rounds){
-      imgSection.removeEventListener('Chart',clickHandler);
-      ShowResults();
-    }      
-      function ShowResults(event){
-      let CCart=document.getElementById('CChart');
-      let CChart={
-      
-      type:'bar',
-      data:{
-        label:'views',
-     datasets:[{
-       backgroundColor:'#3cb44b',
-       data:'views',
-     },
-    {
-     label:'votes',
-     data:'votes',
-    }]
-      }
-
-      }
-      let chart = new Chart(CCart, CChart);
-
-      }
-
+      imgSection.removeEventListener('click',clickHandler);
+      alert('you are out of click :)')
+      creatchart();
     }
+  }
+  function creatchart(){
+    let context = document.getElementById('myChart').getContext('2d');
+    let productName=[];
+    let productVotes=[];
+    let productViews=[];
+    for(let i=0;i<Products.all.length;i++){
+      productName.push(Products.all[i].name);
+    }
+    for(let i=0;i<Products.all.length;i++){
+      productVotes.push(Products.all[i].votes);
+    }
+    for(let i=0;i<Products.all.length;i++){
+      productViews.push(Products.all[i].views);
+    }
+    let chartObject={
+      type: 'bar',
+
+      // The data for our dataset
+      data: {
+          labels: productName,
+          datasets: [{
+              label: 'number of votes',
+              backgroundColor: 'rgb(88, 63, 63)',
+              borderColor: 'rgb(255, 99, 132)',
+              data: productVotes,
+            },
+            {
+            label: 'number of Views',
+            backgroundColor: 'rgb(247, 206, 206)',
+            borderColor: 'rgb(129, 24, 24)',
+            data: productViews,
+          }],
+      },
   
+    }
+    local();
+
+    let chart = new Chart(context,chartObject);
+  }
+
+  function local(){
+    let ProductsMall=JSON.stringify(Products.all);
+    localStorage.setItem('Data',ProductsMall);
+  }
+  function data(){
+    let allData =localStorage.getItem('Data');
+    if(allData){
+      Products.all=JSON.parse(allData);
+    }
+  }
